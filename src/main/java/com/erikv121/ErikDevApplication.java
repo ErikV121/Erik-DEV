de575e6086590015981a6873fa38c00f5b167144
ErikV121 <ErikVasquez121@gmail.com>
2025-11-16 23:34:21 -0500

    Initial Commit

---
+++ src/main/java/com/erikv121/ErikDevApplication.java
@@ -0,0 +1,13 @@
+package com.erikv121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErikDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErikDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erikv121/ErikDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erikv121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErikDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
